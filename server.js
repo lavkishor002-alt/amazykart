490514b8838125d370d50c3a70fbac98bd873d57
lavkishor002-alt <lavkishor002@gmail.com>
2025-09-01 23:45:03 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+
+```js
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Amazon affiliate site is live!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+```
