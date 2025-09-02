89ac057915414ca6eecaeec5fae3ea39102f7b66
lavkishor002-alt <lavkishor002@gmail.com>
2025-09-02 07:54:54 +0530

    server.js

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 
 ```js
-const express = require("express");
+const express = require('express');
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Amazon affiliate site is live!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
-  js
-console.log(`Server is running on port ${PORT}`);
-
+  console.log(`Server is running on port ${PORT}`);
+});
+```
